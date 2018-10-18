9b579eaf663244128ce4d9a7259b78b3c48c5f37
rene <rene.catapang@gmail.com>
2018-10-19 00:05:14 +0800

    alter the main spring boot application

--- src/main/java/com/example/demospringbootintellij/DemospringbootintellijApplication.java
+++ src/main/java/com/example/demospringbootintellij/DemospringbootintellijApplication.java
@@ -2,11 +2,19 @@ package com.example.demospringbootintellij;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController("/")
 @SpringBootApplication
 public class DemospringbootintellijApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemospringbootintellijApplication.class, args);
+	}
+
+	@GetMapping("/home")
+	public String getHome(){
+		return "home";
 	}
 }
